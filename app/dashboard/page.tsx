8593e9e91c5c85566efcbb51ff2e4d782dc1c86c
WINSTEN COELLINS <62231201@wil.local>
2024-04-08 17:18:16 +0700

    Modified table

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -37,27 +37,102 @@ export default function Dashboard () {
             <div>
                 <div>
                     <h1>Transaction History</h1>
-                    <table className="table-auto">
+                    <table className="items-center bg-transparent w-full border-collapse ">
                         <thead>
                         <tr>
-                            <th className='text-left'>Description</th>
-                            <th className='text-left'>Type</th>
-                            <th className='text-left'>Date</th>
-                            <th className='text-left'>Amount</th>
+                            <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                        Page name
+                                        </th>
+                        <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                        Visitors
+                                        </th>
+                        <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                        Unique users
+                                        </th>
+                        <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                        Bounce rate
+                                        </th>
                         </tr>
                         </thead>
+
                         <tbody>
-                            {
-                                transactions.map((t) => (
-                                    <tr key={t.id}>
-                                        <td className="text-left">{t.desc}</td>
-                                        <td className="text-left">{t.type}</td>
-                                        <td className="text-left">{t.date}</td>
-                                        <td className="text-left">{t.amount}</td>
-                                    </tr>
-                                ))
-                            }
+                        <tr>
+                            <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">
+                            /argon/
+                            </th>
+                            <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 ">
+                            4,569
+                            </td>
+                            <td className="border-t-0 px-6 align-center border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                            340
+                            </td>
+                            <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                            <i className="fas fa-arrow-up text-emerald-500 mr-4"></i>
+                            46,53%
+                            </td>
+                        </tr>
+                        <tr>
+                            <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700">
+                            /argon/index.html
+                            </th>
+                            <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                            3,985
+                            </td>
+                            <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                            319
+                            </td>
+                            <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                            <i className="fas fa-arrow-down text-orange-500 mr-4"></i>
+                            46,53%
+                            </td>
+                        </tr>
+                        <tr>
+                            <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700">
+                            /argon/charts.html
+                            </th>
+                            <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                            3,513
+                            </td>
+                            <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                            294
+                            </td>
+                            <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                            <i className="fas fa-arrow-down text-orange-500 mr-4"></i>
+                            36,49%
+                            </td>
+                        </tr>
+                        <tr>
+                            <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700">
+                            /argon/tables.html
+                            </th>
+                            <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                            2,050
+                            </td>
+                            <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                            147
+                            </td>
+                            <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                            <i className="fas fa-arrow-up text-emerald-500 mr-4"></i>
+                            50,87%
+                            </td>
+                        </tr>
+                        <tr>
+                            <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700">
+                            /argon/profile.html
+                            </th>
+                            <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                            1,795
+                            </td>
+                            <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                            190
+                            </td>
+                            <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                            <i className="fas fa-arrow-down text-red-500 mr-4"></i>
+                            46,53%
+                            </td>
+                        </tr>
                         </tbody>
+
                     </table>
                 </div>
 
